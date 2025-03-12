4b72dbebfd04a09893944120a564008b9a7d06ea
alanleew <alanleew412@gmail.com>
2025-03-12 13:41:05 -0700

    Added comments on sections and their purposes

--- main.py
+++ main.py
@@ -4,28 +4,36 @@ from bs4 import BeautifulSoup
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-# date = input("What date would you like to travel to? (YYYY-MM-DD): ")
-# url = f"https://www.billboard.com/charts/hot-100/{date}"
-# url = f"https://www.billboard.com/charts/hot-100/2020-02-02"
-# header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
-#
-# response = requests.get(url, headers=header)
-# response.raise_for_status()
-#
-# html = response.text
-# soup = BeautifulSoup(html, "html.parser")
-#
-# # song_list = soup.select('div.o-chart-results-list-row-container li.lrv-u-width-100p h3#title-of-a-story')
-# song_list = soup.select('div ul li ul li h3')
-# song_titles = [song.getText().strip() for song in song_list]
-# # print(len(song_titles))
-# pprint.pprint(song_titles)
+##############################################################################
+### This section takes an input and scrapes Billboard 100 HTML for the song titles pertaining to a specific date ###
+date = input("What date would you like to travel to? (YYYY-MM-DD): ")
+url = f"https://www.billboard.com/charts/hot-100/{date}"
+url = f"https://www.billboard.com/charts/hot-100/2020-02-02"
+header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
 
+response = requests.get(url, headers=header)
+response.raise_for_status()
+
+html = response.text
+soup = BeautifulSoup(html, "html.parser")
+
+# song_list = soup.select('div.o-chart-results-list-row-container li.lrv-u-width-100p h3#title-of-a-story')
+song_list = soup.select('div ul li ul li h3')
+
+### This contains the 100 song titles from the date input
+song_titles = [song.getText().strip() for song in song_list]
+# print(len(song_titles))
+pprint.pprint(song_titles)
+
+
+### This section uses Spotipy to connect to Spotify ###
 SPOTIFY_CLIENT_ID = "8b23f792cb2c491c93b3d84e84b90022"
 SPOTIFY_CLIENT_SECRET = "c9a64762489e4f6cb2d1afb06ede63ea"
 # APP_REDIRECT_URI = "http://localhost:1234"
 APP_REDIRECT_URI = "https://example.com"
 
+##############################################################################
+### This section authenticates Spotipy with Spotify ###
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
         client_id=SPOTIFY_CLIENT_ID,
